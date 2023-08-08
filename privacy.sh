3130194c76c30edbe3c46770baa5051a1019fc86
lnxd <48756329+lnxd@users.noreply.github.com>
2023-08-08 12:46:25 +1000

    Update & upgrade packages

--- privacy.sh
+++ privacy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # This script automates some of the privacy changes I usually make on fresh Debian installs when I'm working under an NDA
 
+# Update & upgrade packages to avoid repetition
+sudo apt-get update
+sudo apt-get upgrade -y
+
 # Set Cloudflare as default DNS provider on all interfaces
 echo "- Setting DNS provider to Cloudflare on all interfaces"
 interfaces=$(ls /sys/class/net)
