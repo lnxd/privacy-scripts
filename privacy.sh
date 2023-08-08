c5e1d8b1140600ec1fdd7e65459a53dcb5f3de54
lnxd <48756329+lnxd@users.noreply.github.com>
2023-08-08 12:57:28 +1000

    Status checks

--- privacy.sh
+++ privacy.sh
@@ -29,3 +29,36 @@ for iface in $interfaces; do
   fi
 done
 echo "- Done setting DNS provider to Cloudflare on all interfaces"
+
+echo "- Checking Apparmor status"
+apparmor_status=$(cat /sys/module/apparmor/parameters/enabled)
+if [ "$apparmor_status" = "Y" ]; then
+    echo -e "\033[32m- Apparmor is enabled\033[0m"  # 32 for green, 0 to reset the color
+else
+    echo -e "\033[31m- Apparmor is disabled\033[0m"  # 31 for red, 0 to reset the color
+fi
+
+echo "- Checking if DNS provider is Cloudflare"
+dns_provider=$(cat /etc/resolv.conf | grep "nameserver 1.1.1.1")
+if [ -n "$dns_provider" ]; then
+    echo -e "\033[32m- DNS configured correctly\033[0m"
+else
+    echo -e "\033[31m- DNS not configured correctly\033[0m"
+fi
+
+echo "- Checking if firewall is enabled"
+ufw_status=$(sudo ufw status | grep "Status: active")
+if [ -n "$ufw_status" ]; then
+    echo -e "\033[32m- UFW is enabled\033[0m"
+else
+    echo -e "\033[31m- UFW is disabled\033[0m"
+fi
+
+echo "- Checking if package survey is disabled"
+package_survey=$(dpkg -l | grep popularity-contest)
+if [ -z "$package_survey" ]; then
+    echo -e "\033[32m- Package survey is disabled\033[0m"
+else
+    echo -e "\033[31m- Package survey is enabled\033[0m"
+fi
+
