a82ed667b5322a3af6bab53289570698b6ae592b
lnxd <48756329+lnxd@users.noreply.github.com>
2023-08-08 13:06:48 +1000

    Updated output

--- privacy.sh
+++ privacy.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 # This script automates some of the privacy changes I usually make on fresh Debian installs when I'm working under an NDA
 
+echo "This script will make the following changes to your system:"
+echo "- Update & upgrade packages"
+echo "- Disable package survey"
+echo "- Enable UFW"
+echo "- Set Cloudflare as default DNS provider on all interfaces"
+echo "- Enable Apparmor"
+# Check if script is being run as root
+if [ "$EUID" -ne 0 ]; then
+    echo -e "\nError: This script must be run as root\n"
+    exit 1
+fi
+echo -e "\nPlease visit this link and review the script before continuing: https://raw.githubusercontent.com/lnxd/debian-privacy-automations/main/privacy.sh"
+echo "Enter 'y' to continue or 'n' to exit"
+read -r response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
+    echo "- Continuing..."
+else
+    echo "- Exiting..."
+    exit 1
+fi
+
 # Update & upgrade packages to avoid repetition
 sudo apt-get update
 sudo apt-get upgrade -y
@@ -30,15 +51,15 @@ for iface in $interfaces; do
 done
 echo "- Done setting DNS provider to Cloudflare on all interfaces"
 
-echo "- Checking Apparmor status"
+echo -e "\nPrivacy check complete. Results below:"
+
 apparmor_status=$(cat /sys/module/apparmor/parameters/enabled)
 if [ "$apparmor_status" = "Y" ]; then
-    echo -e "\033[32m- Apparmor is enabled\033[0m"  # 32 for green, 0 to reset the color
+    echo -e "\033[32m- Apparmor is enabled\033[0m"
 else
-    echo -e "\033[31m- Apparmor is disabled\033[0m"  # 31 for red, 0 to reset the color
+    echo -e "\033[31m- Apparmor is disabled\033[0m"
 fi
 
-echo "- Checking if DNS provider is Cloudflare"
 dns_provider=$(cat /etc/resolv.conf | grep "nameserver 1.1.1.1")
 if [ -n "$dns_provider" ]; then
     echo -e "\033[32m- DNS configured correctly\033[0m"
@@ -46,7 +67,6 @@ else
     echo -e "\033[31m- DNS not configured correctly\033[0m"
 fi
 
-echo "- Checking if firewall is enabled"
 ufw_status=$(sudo ufw status | grep "Status: active")
 if [ -n "$ufw_status" ]; then
     echo -e "\033[32m- UFW is enabled\033[0m"
@@ -54,7 +74,6 @@ else
     echo -e "\033[31m- UFW is disabled\033[0m"
 fi
 
-echo "- Checking if package survey is disabled"
 package_survey=$(dpkg -l | grep popularity-contest)
 if [ -z "$package_survey" ]; then
     echo -e "\033[32m- Package survey is disabled\033[0m"
