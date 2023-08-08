068582e44cebb8254f6438a85e66729dd2bcc34a
lnxd <48756329+lnxd@users.noreply.github.com>
2023-08-08 13:20:23 +1000

    More output changes

--- privacy.sh
+++ privacy.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 # This script automates some of the privacy changes I usually make on fresh Debian installs when I'm working under an NDA
 
+# Display script info
 echo "This script will make the following changes to your system:"
 echo "- Update & upgrade packages"
 echo "- Disable package survey"
 echo "- Enable UFW"
 echo "- Set Cloudflare as default DNS provider on all interfaces"
 echo "- Enable Apparmor"
+
 # Check if script is being run as root
 if [ "$EUID" -ne 0 ]; then
     echo -e "\nError: This script must be run as root\n"
     exit 1
 fi
+
+# Check if script is being run on Debian
+if [ ! -f /etc/debian_version ]; then
+    echo -e "\nError: This script is only supported on Debian\n"
+    exit 1
+fi
+
+# Confirm user has reviewed the script
 echo -e "\nPlease visit this link and review the script before continuing: https://raw.githubusercontent.com/lnxd/debian-privacy-automations/main/privacy.sh"
 echo "Enter 'y' to continue or 'n' to exit"
 read -r response
@@ -53,31 +63,60 @@ echo "- Done setting DNS provider to Cloudflare on all interfaces"
 
 echo -e "\nPrivacy check complete. Results below:"
 
+# Function to calculate padding
+calculate_padding() {
+    local message_length=$1
+    local max_length=$2
+    local padding=$((max_length - message_length))
+    printf "%${padding}s"
+}
+
+# Print status of each check
+print_status() {
+    local status=$1
+    local message=$2
+
+    local max_length=15  # Adjust this value as needed for the maximum message length
+    local status_padding=$(calculate_padding ${#status} $max_length)
+    local message_padding=$(calculate_padding ${#message} $max_length)
+
+    if [ "$status" = "Passed" ]; then
+        echo -e "${message}:${message_padding}\033[32m${status}${status_padding}\033[0m"
+    elif [ "$status" = "Failed" ]; then
+        echo -e "${message}:${message_padding}\033[31m${status}${status_padding}\033[0m"
+    else
+        echo "${message}:${message_padding}${status}"
+    fi
+}
+
+# Check Apparmor status
 apparmor_status=$(cat /sys/module/apparmor/parameters/enabled)
 if [ "$apparmor_status" = "Y" ]; then
-    echo -e "\033[32m- Apparmor is enabled\033[0m"
+    print_status "Passed" "Apparmor"
 else
-    echo -e "\033[31m- Apparmor is disabled\033[0m"
+    print_status "Failed" "Apparmor"
 fi
 
+# Check DNS provider
 dns_provider=$(cat /etc/resolv.conf | grep "nameserver 1.1.1.1")
 if [ -n "$dns_provider" ]; then
-    echo -e "\033[32m- DNS configured correctly\033[0m"
+    print_status "Passed" "DNS"
 else
-    echo -e "\033[31m- DNS not configured correctly\033[0m"
+    print_status "Failed" "DNS"
 fi
 
+# Check UFW status
 ufw_status=$(sudo ufw status | grep "Status: active")
 if [ -n "$ufw_status" ]; then
-    echo -e "\033[32m- UFW is enabled\033[0m"
+    print_status "Passed" "Firewall"
 else
-    echo -e "\033[31m- UFW is disabled\033[0m"
+    print_status "Failed" "Firewall"
 fi
 
+# Check package survey
 package_survey=$(dpkg -l | grep popularity-contest)
 if [ -z "$package_survey" ]; then
-    echo -e "\033[32m- Package survey is disabled\033[0m"
+    print_status "Passed" "Telemetrics"
 else
-    echo -e "\033[31m- Package survey is enabled\033[0m"
+    print_status "Failed" "Telemetrics"
 fi
-
