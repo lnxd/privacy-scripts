933bdc1913c2db80b530dcbdc217451e18b3e515
Alex <48756329+lnxd@users.noreply.github.com>
2023-08-08 12:30:42 +1000

    Set DNS provider to Cloudflare

--- privacy.sh
+++ privacy.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+# This script automates some of the privacy changes I usually make on fresh Debian installs when I'm working under an NDA
 
+# Set Cloudflare as default DNS provider on all interfaces
+echo "- Setting DNS provider to Cloudflare on all interfaces"
+interfaces=$(ls /sys/class/net)
+for iface in $interfaces; do
+  if [[ "$iface" != "lo" ]]; then
+    echo "Setting DNS servers for $iface..."
+    sudo resolvectl dns "$iface" 1.1.1.1 1.0.0.1
+  fi
+done
+echo "- Done setting DNS provider to Cloudflare on all interfaces"
